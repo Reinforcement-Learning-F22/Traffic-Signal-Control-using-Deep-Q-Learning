94406f9d94460aae42eb73ab4758d2c49776c07e
Walid-khaled <w.khaled@nu.edu.eg>
2022-11-10 21:26:04 +0300

    Add: Model train class

--- model.py
+++ model.py
@@ -10,3 +10,67 @@ from tensorflow.keras import losses
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import plot_model
 from tensorflow.keras.models import load_model
+
+#############################################################################
+###########################    TRAIN MODEL     ##############################
+#############################################################################
+
+class TrainModel:
+    def __init__(self, num_layers, width, batch_size, learning_rate, input_dim, output_dim, maxMemorySize, minMemorySize):
+        self._inputDim = input_dim
+        self._outputDim = output_dim
+        self._batchSize = batch_size
+        self._learningRate = learning_rate
+        self._model = self.build_model(num_layers, width)
+        # Memory
+        self._samples = []
+        self._maxMemorySize = maxMemorySize
+        self._minMemorySize = minMemorySize
+
+
+    def build_model(self, num_layers, width):
+        """
+        Build and compile a fully connected deep neural network
+        """
+        inputs = keras.Input(shape=(self._inputDim,))
+        x = layers.Dense(width, activation='relu')(inputs)
+        for _ in range(num_layers):
+            x = layers.Dense(width, activation='relu')(x)
+        outputs = layers.Dense(self._outputDim, activation='linear')(x)
+
+        model = keras.Model(inputs=inputs, outputs=outputs, name='my_model')
+        model.compile(loss=losses.mean_squared_error, optimizer=Adam(lr=self._learningRate))
+        return model
+    
+
+    def predict_one(self, state):
+        """
+        Predict the action values from a single state
+        """
+        state = np.reshape(state, [1, self._inputDim])
+        return self._model.predict(state)
+
+
+    def predict_batch(self, states):
+        """
+        Predict the action values from a batch of states
+        """
+        return self._model.predict(states)
+
+
+    def train_batch(self, states, q_sa):
+        """
+        Train the NN using the updated Q-values
+        """
+        self._model.fit(states, q_sa, epochs=1, verbose=0)
+
+
+    def save_model(self, path):
+        """
+        Save the current model in the folder as h5 file and a model architecture summary as png
+        """
+        self._model.save(os.path.join(path, 'trained_model.h5'))
+        plot_model(self._model, to_file=os.path.join(path, 'model_structure.png'), show_shapes=True, show_layer_names=True)
+
+
+

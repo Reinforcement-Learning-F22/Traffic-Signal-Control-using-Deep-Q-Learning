9e684989e9ee6aace9109515c6338947fa545da8
Walid-khaled <w.khaled@nu.edu.eg>
2022-11-11 14:39:41 +0300

    Add: run method to test_simulation file

--- Test_Simulation.py
+++ Test_Simulation.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import timeit
 import os
+from Helpers import Traffic_Route_Generator
 
 # phase codes based on environment.net.xml
 PHASE_NS_GREEN = 0  # action 0 code 00
@@ -21,10 +22,62 @@ class Simulation:
         self._TrafficGen = TrafficGen
         self._step = 0
         self._sumo_cmd = CMD
-        self._max_steps = maxSteps
+        self._maxSteps = maxSteps
         self._green_duration = greenDuration
         self._yellow_duration = yellowDuration
         self._num_states = numStates
         self._num_actions = numActions
         self._reward_episode = []
         self._queue_length_episode = []
+
+
+    def run(self, episode):
+        """
+        Runs the testing simulation
+        """
+        start_time = timeit.default_timer()
+
+        # first, generate the route file for this simulation and set up sumo
+        Traffic_Route_Generator(seed=episode)
+        traci.start(self._sumo_cmd)
+        print("Simulating...")
+
+        # inits
+        self._step = 0
+        self._waiting_times = {}
+        old_total_wait = 0
+        old_action = -1 # dummy init
+
+        while self._step < self._maxSteps:
+
+            # get current state of the intersection
+            current_state = self.get_state()
+
+            # calculate reward of previous action: (change in cumulative waiting time between actions)
+            # waiting time = seconds waited by a car since the spawn in the environment, cumulated for every car in incoming lanes
+            current_total_wait = self.collect_waiting_times()
+            reward = old_total_wait - current_total_wait
+
+            # choose the light phase to activate, based on the current state of the intersection
+            action = self.choose_action(current_state)
+
+            # if the chosen phase is different from the last phase, activate the yellow phase
+            if self._step != 0 and old_action != action:
+                self.set_yellow_phase(old_action)
+                self.simulate(self._yellow_duration)
+
+            # execute the phase selected before
+            self.set_green_phase(action)
+            self.simulate(self._green_duration)
+
+            # saving variables for later & accumulate reward
+            old_action = action
+            old_total_wait = current_total_wait
+
+            self._reward_episode.append(reward)
+
+        #print("Total reward:", np.sum(self._reward_episode))
+        traci.close()
+        simulation_time = round(timeit.default_timer() - start_time, 1)
+
+        return simulation_time

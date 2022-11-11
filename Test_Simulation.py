73c4fe6ad70b605225822bebaf6280317e167e47
“SibaIssa” <siba.a.issa@gmail.com>
2022-11-11 14:44:34 +0300

    Add: functions to calculate the reward in test_sim

--- Test_Simulation.py
+++ Test_Simulation.py
@@ -81,3 +81,161 @@ class Simulation:
         simulation_time = round(timeit.default_timer() - start_time, 1)
 
         return simulation_time
+
+    def simulate(self, steps_todo):
+        """
+        Proceed with the simulation in sumo
+        """
+        if (self._step + steps_todo) >= self._maxSteps:  # do not do more steps than the maximum allowed number of steps
+            steps_todo = self._maxSteps - self._step
+
+        while steps_todo > 0:
+            traci.simulationStep()  # simulate 1 step in sumo
+            self._step += 1 # update the step counter
+            steps_todo -= 1
+            queue_length = self.get_queue_length() 
+            self._queue_length_episode.append(queue_length)
+
+
+    def collect_waiting_times(self):
+        """
+        Retrieve the waiting time of every car in the incoming roads
+        """
+        incoming_roads = ["E2TL", "N2TL", "W2TL", "S2TL"]
+        car_list = traci.vehicle.getIDList()
+        for car_id in car_list:
+            wait_time = traci.vehicle.getAccumulatedWaitingTime(car_id)
+            road_id = traci.vehicle.getRoadID(car_id)  # get the road id where the car is located
+            if road_id in incoming_roads:  # consider only the waiting times of cars in incoming roads
+                self._waiting_times[car_id] = wait_time
+            else:
+                if car_id in self._waiting_times: # a car that was tracked has cleared the intersection
+                    del self._waiting_times[car_id] 
+        total_waiting_time = sum(self._waiting_times.values())
+        return total_waiting_time
+
+
+    def choose_action(self, state):
+        """
+        Pick the best action known based on the current state of the env
+        """
+        return np.argmax(self._Model.predict_one(state))
+
+
+    def set_yellow_phase(self, old_action):
+        """
+        Activate the correct yellow light combination in sumo
+        """
+        yellow_phase_code = old_action * 2 + 1 # obtain the yellow phase code, based on the old action (ref on environment.net.xml)
+        traci.trafficlight.setPhase("TL", yellow_phase_code)
+
+
+    def set_green_phase(self, action_number):
+        """
+        Activate the correct green light combination in sumo
+        """
+
+
+        if action_number == 0:
+            traci.trafficlight.setPhase("TL", PHASE_NS_GREEN)
+        elif action_number == 1:
+            traci.trafficlight.setPhase("TL", PHASE_NSL_GREEN)
+        elif action_number == 2:
+            traci.trafficlight.setPhase("TL", PHASE_EW_GREEN)
+        elif action_number == 3:
+            traci.trafficlight.setPhase("TL", PHASE_EWL_GREEN)
+
+
+    def get_queue_length(self):
+        """
+        Retrieve the number of cars with speed = 0 in every incoming lane
+        """
+        halt_N = traci.edge.getLastStepHaltingNumber("N2TL")
+        halt_S = traci.edge.getLastStepHaltingNumber("S2TL")
+        halt_E = traci.edge.getLastStepHaltingNumber("E2TL")
+        halt_W = traci.edge.getLastStepHaltingNumber("W2TL")
+        queue_length = halt_N + halt_S + halt_E + halt_W
+        return queue_length
+
+
+    def get_state(self):
+        """
+        Retrieve the state of the intersection from sumo, in the form of cell occupancy
+        """
+        state = np.zeros(self._num_states)
+        car_list = traci.vehicle.getIDList()
+
+        for car_id in car_list:
+            lane_pos = traci.vehicle.getLanePosition(car_id)
+            lane_id = traci.vehicle.getLaneID(car_id)
+            lane_pos = 750 - lane_pos  # inversion of lane pos, so if the car is close to the traffic light -> lane_pos = 0 --- 750 = max len of a road
+
+            # distance in meters from the traffic light -> mapping into cells
+            if lane_pos < 7:
+                lane_cell = 0
+            elif lane_pos < 14:
+                lane_cell = 1
+            elif lane_pos < 21:
+                lane_cell = 2
+            elif lane_pos < 28:
+                lane_cell = 3
+            elif lane_pos < 40:
+                lane_cell = 4
+            elif lane_pos < 60:
+                lane_cell = 5
+            elif lane_pos < 100:
+                lane_cell = 6
+            elif lane_pos < 160:
+                lane_cell = 7
+            elif lane_pos < 400:
+                lane_cell = 8
+            elif lane_pos <= 750:
+                lane_cell = 9
+
+            # finding the lane where the car is located 
+            # x2TL_3 are the "turn left only" lanes
+            if lane_id == "W2TL_0" or lane_id == "W2TL_1" or lane_id == "W2TL_2":
+                lane_group = 0
+            elif lane_id == "W2TL_3":
+                lane_group = 1
+            elif lane_id == "N2TL_0" or lane_id == "N2TL_1" or lane_id == "N2TL_2":
+                lane_group = 2
+            elif lane_id == "N2TL_3":
+                lane_group = 3
+            elif lane_id == "E2TL_0" or lane_id == "E2TL_1" or lane_id == "E2TL_2":
+                lane_group = 4
+            elif lane_id == "E2TL_3":
+                lane_group = 5
+            elif lane_id == "S2TL_0" or lane_id == "S2TL_1" or lane_id == "S2TL_2":
+                lane_group = 6
+            elif lane_id == "S2TL_3":
+                lane_group = 7
+            else:
+                lane_group = -1
+
+            if lane_group >= 1 and lane_group <= 7:
+                car_position = int(str(lane_group) + str(lane_cell))  # composition of the two postion ID to create a number in interval 0-79
+                valid_car = True
+            elif lane_group == 0:
+                car_position = lane_cell
+                valid_car = True
+            else:
+                valid_car = False  # flag for not detecting cars crossing the intersection or driving away from it
+
+            if valid_car:
+                state[car_position] = 1  # write the position of the car car_id in the state array in the form of "cell occupied"
+
+        return state
+
+
+    @property
+    def queue_length_episode(self):
+        return self._queue_length_episode
+
+
+    @property
+    def reward_episode(self):
+        return self._reward_episode
+
+
+
